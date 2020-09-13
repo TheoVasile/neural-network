1512b0e8e382afb45c4a8debaea06996a2769d8d
TheoVasile <theo.vasile50@yahoo.com>
2020-09-13 16:55:04 -0400

    Initialized layer class

--- artificial_nn.py
+++ artificial_nn.py
@@ -4,3 +4,7 @@ class Node:
     def __init__(self, bias = random.random() * 2 - 1):
         self.value = 0
         self.bias = bias
+
+class Layer:
+    def __init__(self, nodesList):
+        self.nodesList = nodesList
