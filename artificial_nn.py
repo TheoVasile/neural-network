91d5795a67939ea79de77d8d0f12f149b12a683c
TheoVasile <theo.vasile50@yahoo.com>
2021-02-10 10:17:09 -0500

    cleaned some code

--- artificial_nn.py
+++ artificial_nn.py
@@ -1,4 +1,8 @@
-import random, math
+from typing import List, Dict, Tuple
+import random
+import math
+
+VARIABILITY = 2
 
 def sigmoid(z):
     """
@@ -10,119 +14,159 @@ def sigmoid(z):
     """
     return math.sqrt(1 / (1 + math.e ** (-z)))
 
-def test(iterations):
-    """
-    prints neural net output for a set amount of iterations. The neural net
-    should take in 2 inputs (1 or 0), and is expected to return 1 if they are the same, or 0 if
-    they are different. The neural network should learn this pattern
-    """
-    net = Network([Layer(2), Layer(3), Layer(1)])
-    for x in range(iterations):
-        input = [random.randint(0, 1), random.randint(0, 1)]
-        output = [int(input[0] == input[1])]
-        print(net.feedForward(input).values(), output)
-        net.backPropogate(output)
 
 class Node:
     """
     A single node within the network
+
+    === Attributes ==
+    value: the value a node holds at any given moment
+    bias: the bias the node adds to its value
     """
-    def __init__(self, bias = random.random() * 2 - 1):
+    # attribute types
+    value: float
+    bias: float
+
+    def __init__(self):
+        """
+        Initialize node
+        """
         self.value = 0
-        self.bias = bias
+        self.bias = random.random() * VARIABILITY - VARIABILITY / 2
+
 
 class Layer:
     """
     A single layer within the network that contains its own nodes
+
+    === Attributes ===
+    nodesList: a list containing all the nodes in the layer
+    length: the amount of nodes in the layer
     """
-    def __init__(self, nodesLength : int):
-        self.nodesList = [Node() for node in range(0, nodesLength)]
-        self.length = nodesLength
-    def nodeAt(self, j):
-        return self.nodesList[j]
-    def values(self):
+    # attribute types
+    nodes_list: List[Node]
+    length: int
+
+    def __init__(self, length: int):
+        """
+        Initialize layer
+
+        precondition: the length of <nodes_list> must be greater than 0
+        """
+        self.nodes_list = [Node() for node in range(0, length)]
+        self.length = length
+
+    def node_at(self, j: int) -> Node:
+        """
+        Return the node at position {j} in the layer
+        """
+        return self.nodes_list[j]
+
+    def values(self) -> List[float]:
         """
         Return all the node values in the list
         """
-        return [node.value for node in self.nodesList]
-    def resetValues(self):
-        """
-        Set all the values in the layer to 0
-        """
-        for node in self.nodesList:
-            node.value = 0
+        return [node.value for node in self.nodes_list]
+
 
 class Network:
     """
-    The neural network itself, containing individual layers that contain individual nodes
+    The neural network itself, containing individual layers that contain
+    individual nodes
+
+    === Attributes ===
+    learning_rate: how much an individual change will affect the network
+    layers_list: a list containing all the layers in the network
+    weights: all the weights between nodes in the network
     """
-    def __init__(self, layersList : list):
-        self.learningRate = 0.001
-        self.layersList = layersList
+    # attribute types
+    learning_rate: float
+    layers_list: List[Layer]
+    weights: Dict[int, Dict[Tuple[int, int], float]]
+
+    def __init__(self, layers_list: list):
+        """
+        initialize the network
+
+        precondition: the length of <layers_list> must be greater than
+        or equal to 2
+        """
+        self.learning_rate = 0.002
+        self.layers_list = layers_list
         self.weights = {}
-        for l in range(0, len(layersList)):
-            currentLayer = self.layersList[l]
-            previousLayer = self.layersList[l-1]
-            self.weights[l] = {}
-            for j in range(currentLayer.length):
-                for k in range(0, previousLayer.length):
-                    self.weights[l][(k, j)] = random.random() * 2 - 1
-        print(self.weights)
-    def feedForward(self, inputValues):
+        for layer in range(0, len(layers_list)):
+            current_layer = self.layers_list[layer]
+            previous_layer = self.layers_list[layer - 1]
+            self.weights[layer] = {}
+            for j in range(current_layer.length):
+                for k in range(0, previous_layer.length):
+                    self.weights[layer][(
+                        k, j)] = random.random() * VARIABILITY - VARIABILITY / 2
+
+    def feed_forward(self, input_values: List[float]) -> Layer:
         """
         return the output of the neural network based on a specified input
+
+        precondition: the length of <input_values> must be greater than 0
         """
-        for i in range(self.layersList[0].length):
-            node = self.layersList[0].nodeAt(i)
-            node.value = inputValues[i]
-            node.value += node.bias
-            node.value = sigmoid(node.value)
+        for i in range(self.layers_list[0].length):
+            node = self.layers_list[0].node_at(i)
+            node.value = input_values[i]
 
-        for l in range(1, len(self.layersList)):
-            currentLayer = self.layersList[l]
-            previousLayer = self.layersList[l - 1]
-            for j in range(currentLayer.length):
-                currentNode = currentLayer.nodeAt(j)
-                for k in range(previousLayer.length):
-                    previousNode = previousLayer.nodeAt(k)
-                    currentNode.value += previousNode.value * self.weights[l][(k, j)]
-                currentNode.value += currentNode.bias
-                currentNode.value = sigmoid(currentNode.value)
+        for layer in range(1, len(self.layers_list)):
+            current_layer = self.layers_list[layer]
+            previous_layer = self.layers_list[layer - 1]
+            for j in range(current_layer.length):
+                current_node = current_layer.node_at(j)
+                current_node.value = 0
+                for k in range(previous_layer.length):
+                    previous_node = previous_layer.node_at(k)
+                    current_node.value += previous_node.value * \
+                                          self.weights[layer][(k, j)]
+                current_node.value += current_node.bias
+                current_node.value = sigmoid(current_node.value)
 
-        return self.layersList[-1]
-    def backPropogate(self, outputValues):
-        derivatives = {}
-        derivatives[len(self.layersList) - 1] = {}
+        return self.layers_list[-1]
 
-        #iterate backwards through layers
-        for l in range(len(self.layersList) - 1, 0, -1):
-            currentLayer = self.layersList[l]
-            nextLayer = self.layersList[l - 1]
-            derivatives[l-1] = {}
+    def back_propagate(self, output_values: List[float]) -> None:
+        """
+        Mutate the weights and biases of the network based on a given output
 
-            #iterate through the nodes in the layer behind the current layer
-            for k in range(self.layersList[l - 1].length):
-                nextNode = nextLayer.nodeAt(k)
-                derivatives[l - 1][k] = 0
-                print(f"a[{l-1}][{k}] = {nextNode.value}")
+        Precondition: the length of <output_values> must be greater than 0
+        """
+        derivatives = {len(self.layers_list) - 1: {}}
 
-                #iterate through the nodes in the current layer
-                for j in range(self.layersList[l].length):
-                    currentNode = currentLayer.nodeAt(j)
+        # iterate backwards through layers
+        for layer in range(len(self.layers_list) - 1, 0, -1):
+            current_layer = self.layers_list[layer]
+            next_layer = self.layers_list[layer - 1]
+            derivatives[layer - 1] = {}
 
-                    if l == len(self.layersList) - 1:
-                        derivatives[l][j] = -2 * (outputValues[j] - currentNode.value) * currentNode.value * (1 - currentNode.value)
-                        print(f"    d[{l}][{j}] = -2 * (t[{j}] - a[{l}][{j}]) * a[{l}][{j}] * (1 - a[{l}][{j}])")
-                        currentNode.bias -= self.learningRate * derivatives[l][j]
-                        print(f"    b[{l}][{j}] -= n * d[{l}][{j}]")
-                    derivatives[l - 1][k] += derivatives[l][j] * self.weights[l][(k, j)] * nextNode.value * (1 - nextNode.value)
+            # iterate through the nodes in the layer behind the current layer
+            for k in range(self.layers_list[layer - 1].length):
+                next_node = next_layer.node_at(k)
+                derivatives[layer - 1][k] = 0
 
-                    print(f"    d[{l-1}][{k}] += d[{l}][{j}] * w[{l}][({k},{j})] * a[{l-1}][{k}] * (1 - a[{l-1}][{k}]) = {derivatives[l][j] * self.weights[l][(k, j)] * nextNode.value * (1 - nextNode.value)}")
-                    self.weights[l][(k, j)] -= self.learningRate * derivatives[l][j] * nextNode.value
-                    print(f"    w[{l}][({k},{j})] -= n * d[{l}][{j}] * a[{l-1}][{k}]")
+                # iterate through the nodes in the current layer
+                for j in range(self.layers_list[layer].length):
+                    current_node = current_layer.node_at(j)
 
-                nextNode.bias -= self.learningRate * derivatives[l - 1][k]
-                print(f"b[{l - 1}][{k}] -= n * d[{l - 1}][{k}]")
-        print(derivatives)
+                    if layer == len(self.layers_list) - 1:
+                        derivatives[layer][j] = -2 * (output_values[j] -
+                                                      current_node.value) * \
+                                                current_node.value * (
+                                                        1 - current_node.value)
+                        current_node.bias -= self.learning_rate * \
+                                             derivatives[layer][
+                                                 j]
+                    derivatives[layer - 1][k] += derivatives[layer][j] * \
+                                                 self.weights[layer][
+                                                     (k,
+                                                      j)] * next_node.value * (
+                                                         1 - next_node.value)
 
-test(1)
+                    self.weights[layer][(k, j)] -= self.learning_rate * \
+                                                   derivatives[layer][
+                                                       j] * next_node.value
+
+                next_node.bias -= self.learning_rate * derivatives[layer - 1][k]
