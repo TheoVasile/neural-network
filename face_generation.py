49876d1038c23e7fda7580f9d9c771606717c3e9
TheoVasile <theo.vasile50@yahoo.com>
2021-02-10 11:21:53 -0500

    network can now be displayed in color or grayscale, with different image dimensions

--- face_generation.py
+++ face_generation.py
@@ -10,16 +10,27 @@ pg.display.set_caption("face generation")
 clock = pg.time.Clock()
 fps = 60
 
-def display(pixels: List[float]) -> None:
-    n = int(min(W, H) / 32)
-    for i in range(0, len(pixels), 3):
-        y = int(i / (3 * 32))
-        x = (i - (3 * 32) * y) / 3
-        pg.draw.rect(screen, (int(pixels[i] * 255), int(pixels[i+1] * 255),
-                              int(pixels[i+2] * 255)), (int(x * n), int(y * n),
-                                                        n, n), 0)
 
-network = Network([Layer(10), Layer(100), Layer(3072)])
+def display(pixels: List[float], side_length: int, rgb: bool = True) -> None:
+    """
+    Draws pixels to the screen based on a a list <pixels> which contains
+    information about the color of a pixel on screen so that it creates
+    a square image of a certain <side_length>, with or without <rgb> color.
+    """
+    n = int(min(W, H) / side_length)
+    color_channels = (2 * rgb + 1)
+    for i in range(0, len(pixels), color_channels):
+        y = int(i / (color_channels * 32))
+        x = (i - (color_channels * 32) * y) / color_channels
+        if rgb:
+            color = (int(pixels[i] * 255), int(pixels[i+1] * 255),
+                     int(pixels[i+2] * 255))
+        else:
+            color = [int(pixels[i] * 255)] * 3
+        pg.draw.rect(screen, color, (int(x * n), int(y * n), n, n), 0)
+
+
+network = Network([Layer(10), Layer(100), Layer(1024)])
 running = True
 while running:
     for event in pg.event.get():
@@ -30,7 +41,7 @@ while running:
 
     inputs = [random.random() for x in range(10)]
     output = network.feed_forward(inputs).values()
-    display(output)
+    display(output, 32,)
 
     pg.display.update()
     clock.tick(fps)
