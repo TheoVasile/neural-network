b1326391bece987ac71264ffa50029a3cdf753ef
TheoVasile <theo.vasile50@yahoo.com>
2021-02-10 10:54:44 -0500

    added a neural network and a method of displaying its output as an image

--- face_generation.py
+++ face_generation.py
@@ -1,4 +1,7 @@
 import pygame as pg
+from artificial_nn import Network, Layer
+import random
+from typing import List
 
 W = 200
 H = 200
@@ -7,6 +10,16 @@ pg.display.set_caption("face generation")
 clock = pg.time.Clock()
 fps = 60
 
+def display(pixels: List[float]) -> None:
+    n = int(min(W, H) / 32)
+    for i in range(0, len(pixels), 3):
+        y = int(i / (3 * 32))
+        x = (i - (3 * 32) * y) / 3
+        pg.draw.rect(screen, (int(pixels[i] * 255), int(pixels[i+1] * 255),
+                              int(pixels[i+2] * 255)), (int(x * n), int(y * n),
+                                                        n, n), 0)
+
+network = Network([Layer(10), Layer(100), Layer(3072)])
 running = True
 while running:
     for event in pg.event.get():
@@ -15,7 +28,9 @@ while running:
 
     screen.fill((0, 0, 0))
 
-    ...
+    inputs = [random.random() for x in range(10)]
+    output = network.feed_forward(inputs).values()
+    display(output)
 
     pg.display.update()
     clock.tick(fps)
